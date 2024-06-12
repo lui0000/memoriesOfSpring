b710a53011551e1152e38cb4aa2406d4fb97e8fe
Lisa <elbondarenko04@gmail.com>
2024-06-12 23:29:59 +0300

    dependency injection part 2

--- src/main/java/com/example/MusicPlayer.java
+++ src/main/java/com/example/MusicPlayer.java
@@ -2,12 +2,34 @@ package com.example;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
+
+    public MusicPlayer() {}
+    //Ioc
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
-    }
+    } 
 
 }
