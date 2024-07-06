03828f68620d84fc0b6a123d9bbbdea509ebab6f
Lisa <elbondarenko04@gmail.com>
2024-07-06 15:16:07 +0300

    singleton

--- src/main/java/com/example/TestSpring.java
+++ src/main/java/com/example/TestSpring.java
@@ -8,10 +8,19 @@ public class TestSpring {
             "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        
+        boolean comp = musicPlayer1 == musicPlayer2;
+        System.out.println(comp);
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+        musicPlayer1.setVolume(10);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
+        // musicPlayer.playMusicList();
+        // System.out.println(musicPlayer.getName());
+        // System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
